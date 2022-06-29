c899718ef88b62551634307de7003eea09d224a6
Carlos Eduardo Prezzi <carlosedprezzi@gmail.com>
2022-06-29 00:50:25 -0300

    Permitir tile sem texto

--- src/br/univali/poo/termigame/TileMap.java
+++ src/br/univali/poo/termigame/TileMap.java
@@ -63,6 +63,7 @@ public class TileMap extends Drawable {
 
     private void organizeTileset() {
         this.tileSet = new HashMap<>();
+        this.tileSet.put("", Text.class.getName());
         this.tileSet.put(" ", Text.class.getName());
         this.tileSet.put("wall", Wall.class.getName());
     }
